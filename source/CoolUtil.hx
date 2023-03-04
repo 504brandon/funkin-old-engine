369f18e57ad675bfd76a120a0d33a54123b4a12a
504brandon <mb.bot.official@gmail.com>
2023-03-04 00:29:52 -0600

    first commit

---
+++ source/CoolUtil.hx
@@ -0,0 +1,12 @@
+package;
+
+class CoolUtil
+{
+	public static function truncateFloat(number:Float, precision:Int):Float
+	{
+		var num = number;
+		num = num * Math.pow(10, precision);
+		num = Math.round(num) / Math.pow(10, precision);
+		return num;
+	}
+}
